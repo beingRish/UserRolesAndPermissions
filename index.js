7337d9db9817ee802eca0af01a5f5f9ddeafb145
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-07-29 07:02:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require ('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/usersRolesPermission");
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const port = process.env.SERVER_PORT | 3000;
+
+app.listen(port, () => {
+    console.log("Server is running on Port: ", port)
+});
